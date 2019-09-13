2d73e8a9cb01fda5413cb59945faddf809ab67e9
masonparsons <masonparsons711@yahoo.com>
2019-09-13 08:40:54 -0700

    pythagorus
    lab 1

---
+++ labs-main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tutorial
+//
+//  Created by Parsons, Mason on 9/3/19.
+//  Copyright © 2019 Parsons, Mason. All rights reserved.
+//
+
+#include <stdio.h>
+#include "pythagorus.h"
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+     pythagorus();
+    return 0;
+}
---
+++ pythagorus.c
@@ -0,0 +1,25 @@
+//
+//  devops.c
+//  tutorial
+//
+//  Created by Parsons, Mason on 9/3/19.
+//  Copyright © 2019 Parsons, Mason. All rights reserved.
+//
+#include <stdio.h>
+#include <math.h>
+int pythagorus(void) {
+    double x,y,z;
+    
+    
+    printf("input value for x");
+    scanf("%lf",&x);
+    printf("imput value for y");
+    scanf("%lf",&y);
+    
+    z=(x*x)+(y*y);
+    z=sqrt(z);
+    printf("%lf",z);
+    return 0;
+}
+
+
