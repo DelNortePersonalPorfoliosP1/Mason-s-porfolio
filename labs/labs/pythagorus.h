63832b1765fbc793e28b7a7c971b8e4f130b43d3
masonparsons <masonparsons711@yahoo.com>
2019-09-17 10:10:09 -0700

    final fix

---
+++ labs/labs/pythagorus.h
@@ -0,0 +1,15 @@
+//
+//  devops.h
+//  tutorial
+//
+//  Created by Parsons, Mason on 9/3/19.
+//  Copyright © 2019 Parsons, Mason. All rights reserved.
+//
+
+#ifndef devops_h
+#define devops_h
+#include <stdio.h>
+int pythagorus(void);
+//a prototype
+#endif /* devops_h */
+
