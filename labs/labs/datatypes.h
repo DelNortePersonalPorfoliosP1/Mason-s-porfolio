d75c17168f6f0c6bee5c9c023a92dbc7a13d0076
masonparsons <masonparsons711@yahoo.com>
2019-09-17 09:31:11 -0700

    new project
    fix?

---
+++ labs/labs/datatypes.h
@@ -0,0 +1,14 @@
+//
+//  lab 2.h
+//  Labs
+//
+//  Created by Parsons, Mason on 9/13/19.
+//  Copyright © 2019 Parsons, Mason. All rights reserved.
+//
+
+#ifndef lab_2_h
+#define lab_2_h
+
+#include <stdio.h>
+int datatypes(void) {
+#endif /* lab_2_h */
